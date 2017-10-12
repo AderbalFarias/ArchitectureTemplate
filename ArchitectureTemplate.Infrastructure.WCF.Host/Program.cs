be7d91bafe754091ce8cea47c97189ea0e2d937f
Aderbal Farias <aderbalfarias@hotmail.com>
2017-10-12 01:07:33 +0100

    Remove of the SimpleInjector

--- ArchitectureTemplate.Infrastructure.WCF.Host/Program.cs
+++ ArchitectureTemplate.Infrastructure.WCF.Host/Program.cs
@@ -6,15 +6,11 @@ namespace ArchitectureTemplate.Infrastructure.WCF.Host
 {
     class Program
     {
-
-
         static void Main(string[] args)
         {
-            //Container container = new Container();
-            //BootstrapperWcf.RegisterServices(container);
-
             ServiceHost hostGeoManager = new ServiceHost(typeof(TelaManager));
             hostGeoManager.Open();
+
 
             Console.WriteLine("Services started. Press [Enter] to exit.");
             Console.ReadLine();
