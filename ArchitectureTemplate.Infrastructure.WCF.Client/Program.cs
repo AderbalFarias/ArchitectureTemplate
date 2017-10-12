23d96e059976bac6a0454930635544f1a158d172
Aderbal Farias <aderbalfarias@hotmail.com>
2017-10-12 01:46:25 +0100

    New project client for wcf and adjust of the project proxies

---
+++ ArchitectureTemplate.Infrastructure.WCF.Client/Program.cs
@@ -0,0 +1,36 @@
+﻿using ArchitectureTemplate.Infrastructure.WCF.Proxies;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+namespace ArchitectureTemplate.Infrastructure.WCF.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetWithTcp();
+        }
+
+        private static void GetWithTcp()
+        {
+            TelaClient proxy = new TelaClient("tcpEP");
+            var data = proxy.GetByName("Usuario");
+
+            proxy.Close();
+        }
+
+        private static void GetWithHttp()
+        {
+            EndpointAddress address = new EndpointAddress("net.tcp://localhost:8009/TelaService");
+            Binding binding = new NetTcpBinding();
+
+            TelaClient proxy = new TelaClient(binding, address);
+            var data = proxy.GetTelas("T");
+            var teste = data.ToList();
+
+            proxy.Close();
+        }
+
+    }
+}
---
+++ ArchitectureTemplate.Infrastructure.WCF.Proxies/TelaClient.cs
@@ -0,0 +1,41 @@
+﻿using ArchitectureTemplate.Infrastructure.WCF.Contracts.Entities;
+using ArchitectureTemplate.Infrastructure.WCF.Contracts.ServiceInterfaces;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+namespace ArchitectureTemplate.Infrastructure.WCF.Proxies
+{
+    public class TelaClient : ClientBase<ITelaServiceContract>, ITelaServiceContract
+    {
+        public TelaClient(string endpointName)
+            : base(endpointName)
+        {
+        }
+
+        public TelaClient(Binding binding, EndpointAddress address)
+            : base(binding, address)
+        {
+        }
+
+        public TelaContract GetById(int id)
+        {
+            return Channel.GetById(id);
+        }
+
+        public TelaContract GetByName(string name)
+        {
+            return Channel.GetByName(name);
+        }
+
+        public IEnumerable<TelaContract> GetTelas(string key)
+        {
+            return Channel.GetTelas(key);
+        }
+
+        public IEnumerable<TelaContract> GetTelas(int idBegin, int idEnd)
+        {
+            return Channel.GetTelas(idBegin, idEnd);
+        }
+    }
+}
